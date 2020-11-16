7bd5ab6c2e76fd4e3dabae8c3716faa505ecaef9
Diyar Kudrat <dkudrat@gmail.com>
2020-11-16 14:19:22 -0800

    implemented user table to CRUD users using DynamoDB

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+const AWS = require('aws-sdk');
+
+const USERS_TABLE = process.env.USERS_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+app.use(bodyParser.json({ strict: false }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!!');
